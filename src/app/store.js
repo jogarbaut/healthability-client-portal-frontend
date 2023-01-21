ffb0325e7cc9e912550e956d364faa0aa71f0e62
Jomel Bautista <jomelgbautista@gmail.com>
2023-01-21 14:09:26 -0800

    deploy packages added

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from './api/apiSlice'
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
-import authReducer from '../features/auth/authSlice'
+import { configureStore } from "@reduxjs/toolkit"
+import { apiSlice } from "./api/apiSlice"
+import { setupListeners } from "@reduxjs/toolkit/dist/query"
+import authReducer from "../features/auth/authSlice"
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer
+    auth: authReducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: false,
 })
 
 setupListeners(store.dispatch)
